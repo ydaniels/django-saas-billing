2c9f6b1bb9b167aaf8dfcd5a9b62a5f2f54078db
yomid4all@gmail.com <yomid4all@gmail.com>
2020-05-01 02:50:05 +0100

    Transaction serializer include subscription name

--- saas_billing/serializers.py
+++ saas_billing/serializers.py
@@ -14,6 +14,13 @@ class SubscriptionTransactionSerializerPayment(serializers.ModelSerializer):
     """SubscriptionTransaction serializer"""
 
     cryptocurrency_payments = CryptoCurrencyPaymentSerializer(many=True, read_only=True)
+    subscription_name = serializers.SerializerMethodField()
+
+    def get_subscription_name(self, obj):
+        if obj.subscription:
+            return '{} {}'.format(obj.subscription.plan_cost.plan.plan_name,
+                                  obj.subscription.plan_cost.display_billing_frequency_text)
+        return ''
 
     class Meta:
         model = SubscriptionTransaction
